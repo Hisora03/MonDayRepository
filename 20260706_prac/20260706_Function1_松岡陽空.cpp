26864446baed4b64f55a98cd3b539e3c580a9821
Hisora03 <sunsky2328@gmail.com>
2026-07-06 11:35:47 +0900

    HP回復

--- 20260706_prac/20260706_Function1_松岡陽空.cpp
+++ 20260706_prac/20260706_Function1_松岡陽空.cpp
@@ -10,14 +10,32 @@ void Contents()
 
 	//画面に表示される言葉
 	cout << "HPを回復する場合は１，回復しない場合は２を入力してください" << endl;
-	cin >> plySelect;
-	Heal(plyHP,plySelect);
+	cin >> plySelect;              //入力
+	Heal(plyHP,plySelect);         //呼び出し
 
 	cout << "あなたのHPは" << plyHP << "です" << endl;
 }
+void InputCheck(int min, int max)
+{
+	int num;
+	while (true)
+	{
+		cin >> num;
+		if (min > num|| max < num)
+		{
+			cout << "入力に誤りがあります。再度入力してください" << endl;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	
+}
 void Heal(int & HP,int plySelect)
 {
-	
+	//HPの回復
 	if (plySelect == 1)
 	{
 		HP += 20;
--- 20260706_prac/20260706_Header1_松岡陽空.h
+++ 20260706_prac/20260706_Header1_松岡陽空.h
@@ -2,11 +2,13 @@
 //定数
 const int HP_MAX = 100;
 const int RECOVERYAMOUNT = 20;
+const int MIN = 1;
+const int MAX = 2;
 
 //関数プロトタイプ宣言
 void Heal(int& HP, int plySelect);
 /// <summary>
 ///入力チェック
 /// </summary>
-
+void InputCheck(int min, int max);
 void Contents();
